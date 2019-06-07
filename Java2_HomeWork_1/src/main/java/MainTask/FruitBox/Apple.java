1e4be6a5d215905d7c6cfc74a36c125750a2d764
Qazss <Alexander.Dvoretskiy@gmail.com>
2019-06-07 16:10:40 +0300

    ДЗ к первому уроку

---
+++ Java2_HomeWork_1/src/main/java/MainTask/FruitBox/Apple.java
@@ -0,0 +1,10 @@
+package MainTask.FruitBox;
+
+public class Apple extends Fruit {
+
+    private static final String name = "Apple";
+
+    public Apple(float weight) {
+        super(name, weight);
+    }
+}
