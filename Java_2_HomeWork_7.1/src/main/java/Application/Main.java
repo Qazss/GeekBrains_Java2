02e87953d401450ffc3d16b6f13b150ed003cd32
dvoretskiy.aleksandr <dvoretskiy.aleksandr@otr.ru>
2019-06-30 11:00:29 +0300

    ДЗ к седьмому уроку Программа для проверки ДЗ Добавил визуальный интерфейс

---
+++ Java_2_HomeWork_7.1/src/main/java/Application/Main.java
@@ -0,0 +1,25 @@
+package Application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        String fxmlFile = "/fxml/sample.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+        primaryStage.setTitle("HomeWork Checker");
+        Scene scene = new Scene(root, 1000, 700);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
